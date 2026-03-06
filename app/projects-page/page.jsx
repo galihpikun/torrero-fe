b98400105fc3aa6f0c4bcaf0622b9c7e0741810c
galihpikun <galihrakha003@gmail.com>
2026-03-06 20:42:27 +0700

    fix: redeploy db restart deployment

--- app/projects-page/page.jsx
+++ app/projects-page/page.jsx
@@ -1,9 +1,6 @@
 import ProjectList from "@/components/custom-components/project-list";
 
-
 export default function projectsPage() {
-    
-
     return(
         <div className="bg-gray-200 flex flex-col h-screen">
             <ProjectList></ProjectList>
